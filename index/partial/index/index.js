5c00822ed8e0365a0c25b83ef7aad9b197e29b5a
Potapov Max <mp@multiship.ru>
2014-10-23 18:47:27 +0400

    search page

--- index/partial/index/index.js
+++ index/partial/index/index.js
@@ -1,16 +1,3 @@
 angular.module('index').controller('IndexCtrl',function($scope, $modal){
 
-  $scope.read = function($event) {
-    if (!$scope.isAuthenticated) {
-      $modal.open({
-        templateUrl: 'landing/registerPrompt/registerPrompt.html',
-        controller: 'RegisterpromptCtrl'
-      }).result.then(function(result){
-          console.log(result);
-        });
-    } else {
-      throw new Error('Not implemented');
-    }
-  };
-
 });
---
+++ landing/directive/registerToRead/registerToRead.js
@@ -0,0 +1,23 @@
+angular.module('landing').directive('registerToRead', function($modal) {
+	return {
+		replace: true,
+		templateUrl: 'landing/directive/registerToRead/registerToRead.html',
+		link: function(scope, element, attrs, fn) {
+
+      scope.read = function($event) {
+        $event.preventDefault();
+        if (!scope.isAuthenticated) {
+          $modal.open({
+            templateUrl: 'landing/registerPrompt/registerPrompt.html',
+            controller: 'RegisterpromptCtrl'
+          }).result.then(function(result){
+              console.log(result);
+            });
+        } else {
+          throw new Error('Not implemented');
+        }
+      };
+
+		}
+	};
+});
